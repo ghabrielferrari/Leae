e1bcbab43d049d61230768ed5cb9bbcf3cbf9611
ghabrielferrarii <ghcferrari75@gmail.com>
2025-05-12 09:12:12 -0300

    Initial Commit

---
+++ Leae/LeaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeaeApp.swift
+//  Leae
+//
+//  Created by Aluno 17 on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LeaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
